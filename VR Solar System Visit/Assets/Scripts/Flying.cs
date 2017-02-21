22b9a01cf437652828e54a958e57883ad6e3d7d7
sohail0992 <sohail9099@yahoo.com>
2017-02-21 12:38:40 +0500

    solarSystemOnGithub

---
+++ VR Solar System Visit/Assets/Scripts/Flying.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flying : MonoBehaviour {
+    public Transform head;
+    public SteamVR_TrackedObject leftHand;
+    public SteamVR_TrackedObject rightHand;
+    private bool isFlying = false;
+	// Update is called once per frame
+	void Update () {
+        var ldevice = SteamVR_Controller.Input((int)leftHand.index);
+        var rdevice = SteamVR_Controller.Input((int)rightHand.index);
+        if (rdevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) || ldevice.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            isFlying = !isFlying;
+        }
+        if (isFlying)
+        {
+            Vector3 leftDir = leftHand.transform.position - head.position;
+            Vector3 rightDir = rightHand.transform.position - head.position;
+            Vector3 dir = leftDir + rightDir;
+            transform.position += (dir *3* .1f);
+        }
+    }
+}
